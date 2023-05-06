c28cb27a0b39f055efc8960b6f6d0cdc8ac37aa7
Bruno Alpachino <brunolalpachino@gmail.com>
2023-05-06 14:32:26 +0300

    finalised on the project set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <P>this is a view</P>
-      </div>
-    </>
+    <div className="container">
+      <HomeScreen />
+    </div>
   );
 }
 
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import "./Home.css";
+import styles from "./Home.module.css";
+const HomeScreen = () => {
+  return (
+    <div className={styles.home}>
+      <p className={styles.container}>This is my box </p>
+    </div>
+  );
+};
+
+export default HomeScreen;
